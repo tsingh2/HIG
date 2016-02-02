5730ffdb0aa66a9c5aba5190e0ab96f107a3f286
Tejpal <tsingh2@seasiainfotech.com>
2016-02-02 15:11:30 +0530

    added code

---
+++ server/app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes');
+var users = require('./routes/user');
+
+var app = express();
+
+// view engine setup
+
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+app.use(app.router);
+
+app.get('/', routes.index);
+app.get('/users', users.list);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+if ('development' == app.get('env')) {
+    // This will change in production since we'll be using the dist folder
+    // This covers serving up the index page
+    app.use(express.static(path.join(__dirname, '../client/.tmp')));
+    app.use(express.static(path.join(__dirname, '../client/app')));
+    app.use(express.errorHandler());
+}
+
+/**
+* Production Settings
+*/
+if ('production' == app.get('env')) {
+    app.use(express.static(path.join(__dirname, '/dist')));
+}
+
+
+module.exports = app;
